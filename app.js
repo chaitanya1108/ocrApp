da33ba877258ea0551ee64002bde22e3acca9fa1
Chaitanya Kumar <chaitanya4real@gmail.com>
2021-07-02 21:24:19 +0530

    fixed tesseractworker

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require ('express');
+//declared all our imports
+const express = require ("express");
 const app = express();
-const fs = require('fs');
-const multer = require ('multer');
-const {TesseractWorker} = require ('tesseract.js');
-const worker = new TesseractWorker();
+const fs = require("fs");
+const multer = require ("multer");
+const createWorker = require("tesseract.js");
 
+//storage
 const storage= multer.diskStorage({
     destination: (req,res,cb) => {
         cb(null,"./uploads");
@@ -13,10 +14,14 @@ const storage= multer.diskStorage({
         cb(null,req.file);
     }
 });
+const upload = multer({storage: storage}).single("bruh");
 
-const upload = multer({storage: storage}).single('bruh');
-
-app.set('view engine', 'ejs');
+//view engine
+app.set("view engine", "ejs");
+//routes
+app.get("/", (req,res) => {
+    res.render("index");
+})
 
 //start up our server
 const PORT= 5000 || process.env.PORT;
