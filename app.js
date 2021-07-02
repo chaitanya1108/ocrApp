19b821563906c91f5a172a0b94d76e4364e51662
Chaitanya Kumar <chaitanya4real@gmail.com>
2021-07-02 21:42:03 +0530

    can upload files and display file info in console

--- app.js
+++ app.js
@@ -7,21 +7,28 @@ const createWorker = require("tesseract.js");
 
 //storage
 const storage= multer.diskStorage({
-    destination: (req,res,cb) => {
+    destination: (req,file,cb) => {
         cb(null,"./uploads");
     },
-    filename: (req,res,cb) => {
-        cb(null,req.file);
+    filename: (req,file,cb) => {
+        cb(null, file.originalname)
     }
 });
 const upload = multer({storage: storage}).single("bruh");
 
 //view engine
 app.set("view engine", "ejs");
+
 //routes
 app.get("/", (req,res) => {
     res.render("index");
-})
+});
+
+app.post("/uploads", (req,res) =>{
+    upload(req,res,err=>{
+        console.log(req.file);
+    })
+});
 
 //start up our server
 const PORT= 5000 || process.env.PORT;
